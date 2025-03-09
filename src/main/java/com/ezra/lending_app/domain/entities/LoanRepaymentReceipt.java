8328115fb725a6aebdd1c6a4f088c549bc126e6f
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 19:55:03 +0300

    Controller definition

--- src/main/java/com/ezra/lending_app/domain/entities/LoanRepaymentReceipt.java
+++ src/main/java/com/ezra/lending_app/domain/entities/LoanRepaymentReceipt.java
@@ -5,11 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 import static com.ezra.lending_app.domain.util.RandomReferenceGenerator.generateReference;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "loan_repayment")
 public class LoanRepaymentReceipt extends BaseEntity {
