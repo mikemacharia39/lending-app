5203ac215d9884497127070e88eb6a73897e8874
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 17:00:58 +0300

    Define service layer operations

---
+++ src/main/java/com/ezra/lending_app/domain/enums/NotificationType.java
@@ -0,0 +1,10 @@
+package com.ezra.lending_app.domain.enums;
+
+public enum NotificationType {
+    SUCCESS_PAYMENT,
+    LOAN_DUE,
+    PAST_DUE,
+    LOAN_APPROVAL,
+    LOAN_REJECTION,
+    GENERAL_NOTIFICATION
+}
