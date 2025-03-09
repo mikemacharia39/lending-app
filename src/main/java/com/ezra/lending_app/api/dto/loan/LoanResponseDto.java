0771ba93f0042c936c299f5fb964a631b033d05f
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 19:55:10 +0300

    Controller definition

---
+++ src/main/java/com/ezra/lending_app/api/dto/loan/LoanResponseDto.java
@@ -0,0 +1,33 @@
+package com.ezra.lending_app.api.dto.loan;
+
+import com.ezra.lending_app.domain.enums.LoanState;
+import com.ezra.lending_app.domain.enums.LoanTerm;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class LoanResponseDto {
+    private String code;
+    private Long customerId;
+    private Long productId;
+    private BigDecimal requestedAmount;
+    private BigDecimal disbursedAmount;
+    private List<LoanFeeDto> loanFees;
+    private BigDecimal fullLoanAmountPlusFees;
+    private LoanTerm loanTermType;
+    private int loanPeriod;
+    private Instant disbursedDate;
+    private Instant dueDate;
+    private LoanState state;
+    private List<LoanInstallmentDto> installment;
+    private List<LoanRepaymentReceiptDto> repayments;
+}
