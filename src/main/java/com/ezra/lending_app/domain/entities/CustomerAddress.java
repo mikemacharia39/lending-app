1b5affb966ead89abf781c7b986fe69f45c028a2
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 19:55:02 +0300

    Controller definition

--- src/main/java/com/ezra/lending_app/domain/entities/CustomerAddress.java
+++ src/main/java/com/ezra/lending_app/domain/entities/CustomerAddress.java
@@ -5,7 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "customer_address")
 public class CustomerAddress extends BaseEntity {
