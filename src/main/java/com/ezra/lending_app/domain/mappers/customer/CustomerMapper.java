7ba48f79b6f1dae76f90f12cc3123139c5ae69ba
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 16:58:09 +0300

    Define address mapper

---
+++ src/main/java/com/ezra/lending_app/domain/mappers/customer/CustomerMapper.java
@@ -0,0 +1,17 @@
+package com.ezra.lending_app.domain.mappers.customer;
+
+import com.ezra.lending_app.api.dto.CustomerRequestDto;
+import com.ezra.lending_app.api.dto.CustomerResponseDto;
+import com.ezra.lending_app.domain.entities.Customer;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(uses = {CustomerAddressMapper.class})
+public abstract class CustomerMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "dateCreated", ignore = true)
+    @Mapping(target = "dateModified", ignore = true)
+    public abstract Customer toEntity(CustomerRequestDto dto);
+
+    public abstract CustomerResponseDto toDto(Customer customer);
+}
