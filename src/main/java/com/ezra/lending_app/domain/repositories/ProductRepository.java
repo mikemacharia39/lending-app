7e19ba6cf88046b23c56e4b4ff7f3c55394e6af3
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 16:59:20 +0300

    Define product repository

---
+++ src/main/java/com/ezra/lending_app/domain/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.ezra.lending_app.domain.repositories;
+
+import com.ezra.lending_app.domain.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByProductCode(String productCode);
+}
