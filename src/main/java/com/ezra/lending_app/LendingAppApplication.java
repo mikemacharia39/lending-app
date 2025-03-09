7da32033ed7eb370071f23d746ee3e4c700603f3
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 09:12:37 +0300

    App lending app setup

---
+++ src/main/java/com/ezra/lending_app/LendingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ezra.lending_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezra/lending_app/LendingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezra.lending_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
