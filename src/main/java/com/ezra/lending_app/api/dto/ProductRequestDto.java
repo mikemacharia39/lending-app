1e262762e448578765b6034984d8f4e5562d6e35
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 17:00:55 +0300

    Define service layer operations

--- src/main/java/com/ezra/lending_app/api/dto/ProductRequestDto.java
+++ src/main/java/com/ezra/lending_app/api/dto/ProductRequestDto.java
@@ -1,24 +1,54 @@
 package com.ezra.lending_app.api.dto;
 
-import com.ezra.lending_app.domain.enums.LoanTerm;
-import com.ezra.lending_app.domain.enums.ProductStatus;
-import com.ezra.lending_app.domain.enums.RepaymentFrequencyType;
-
 import java.math.BigDecimal;
 import java.util.List;
 
+import com.ezra.lending_app.domain.enums.LoanTerm;
+import com.ezra.lending_app.domain.enums.ProductStatus;
+import com.ezra.lending_app.domain.enums.RepaymentFrequencyType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class ProductRequestDto {
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 255)
     private String name;
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 1000)
     private String description;
-    private BigDecimal interestRate;
+    @NotNull
+    @NotBlank
     private String currency;
+    @NotNull
+    @Min(value = 1, message = "Minimum loan amount must be greater than 0")
     private BigDecimal maxLoanAmount;
+    @NotNull
+    @Min(value = 1, message = "Minimum loan amount must be greater than 0")
     private int minLoanTermDuration;
+    @NotNull
     private LoanTerm minLoanTermType;
+    @NotNull
+    @Min(value = 1, message = "Maximum loan term duration must be greater than 0")
     private int maxLoanTermDuration;
+    @NotNull
     private LoanTerm maxLoanTermType;
-    private List<FeeDto> fees;
+    private List<ProductFeeDto> fees;
+    @NotNull
     private ProductStatus status;
+    @NotNull
     private RepaymentFrequencyType repaymentFrequency;
+    @NotNull
     private int gracePeriodAfterLoanDueDateInDays;
 }
