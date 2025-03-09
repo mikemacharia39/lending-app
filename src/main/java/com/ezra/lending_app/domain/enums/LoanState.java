f48ef8a3140ce702b981bcfb68693b6748411843
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 12:12:08 +0300

    Setup load entities

---
+++ src/main/java/com/ezra/lending_app/domain/enums/LoanState.java
@@ -0,0 +1,11 @@
+package com.ezra.lending_app.domain.enums;
+
+public enum LoanState {
+    PENDING_APPROVAL,
+    OPEN,
+    CLOSED,
+    CANCELLED,
+    OVERDUE,
+    WRITTEN_OFF,
+    REJECTED
+}
