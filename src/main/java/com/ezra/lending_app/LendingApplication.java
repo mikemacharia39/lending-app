0b7b70cce33ef405d649b65b5f0ea1aa2c83aede
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 19:54:58 +0300

    Controller definition

--- src/main/java/com/ezra/lending_app/LendingApplication.java
+++ src/main/java/com/ezra/lending_app/LendingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LendingAppApplication {
+public class LendingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LendingAppApplication.class, args);
+		SpringApplication.run(LendingApplication.class, args);
 	}
 
 }
