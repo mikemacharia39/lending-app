5ba99be2b40563b564629a39b7da99b2410a52bf
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 13:12:23 +0300

    Define request, response dto and improvements to entities

---
+++ src/main/java/com/ezra/lending_app/api/dto/LoanRequestDto.java
@@ -0,0 +1,17 @@
+package com.ezra.lending_app.api.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record LoanRequestDto(
+        Long customerId,
+        Long productId,
+        @NotNull
+        @Min(value = 1, message = "The loan amount must be greater than or equal to 1")
+        BigDecimal requestedAmount,
+        @NotNull
+        int loanPeriod
+) {
+}
