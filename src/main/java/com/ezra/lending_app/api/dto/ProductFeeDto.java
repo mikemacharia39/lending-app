053fc37fe52e8656825db5efce82fe01c904014d
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 17:00:55 +0300

    Define service layer operations

--- src/main/java/com/ezra/lending_app/api/dto/ProductFeeDto.java
+++ src/main/java/com/ezra/lending_app/api/dto/ProductFeeDto.java
@@ -6,10 +6,12 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 
-public record FeeDto(
+@Builder
+public record ProductFeeDto(
         @NotBlank
         @NotNull
         @Size(min = 1, max = 255)
