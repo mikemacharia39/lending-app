9dada6dd4fb3b32bd3d4741c642ad909f440e545
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 19:55:04 +0300

    Controller definition

--- src/main/java/com/ezra/lending_app/domain/entities/NotificationTemplate.java
+++ src/main/java/com/ezra/lending_app/domain/entities/NotificationTemplate.java
@@ -6,9 +6,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import static com.ezra.lending_app.domain.util.RandomReferenceGenerator.generateReference;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "notification_template")
 public class NotificationTemplate extends BaseEntity {
