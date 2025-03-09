f1787b7a47ffefe5a3923d75c10bbb1d18629ef7
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-10 00:05:49 +0300

    Define loan installments

---
+++ src/main/java/com/ezra/lending_app/api/controllers/LoanController.java
@@ -0,0 +1,11 @@
+package com.ezra.lending_app.api.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("loans")
+@RestController
+public class LoanController {
+}
