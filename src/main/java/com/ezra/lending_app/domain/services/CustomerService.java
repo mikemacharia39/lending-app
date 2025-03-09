cd6bd5754c11ecedcece6f0f527e908654c900ed
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-09 17:01:00 +0300

    Define service layer operations

---
+++ src/main/java/com/ezra/lending_app/domain/services/CustomerService.java
@@ -0,0 +1,38 @@
+package com.ezra.lending_app.domain.services;
+
+import com.ezra.lending_app.api.dto.CustomerRequestDto;
+import com.ezra.lending_app.api.dto.CustomerResponseDto;
+import com.ezra.lending_app.domain.entities.Customer;
+import com.ezra.lending_app.domain.mappers.customer.CustomerMapper;
+import com.ezra.lending_app.domain.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Transactional
+    public CustomerResponseDto createCustomer(final CustomerRequestDto customerRequestDto) {
+        final Customer customer = customerMapper.toEntity(customerRequestDto);
+        final Customer savedCustomer = customerRepository.save(customer);
+        return customerMapper.toDto(savedCustomer);
+    }
+
+    @Transactional(readOnly = true)
+    public CustomerResponseDto getCustomer(final String customerCode) {
+        final Optional<Customer> customer = customerRepository.findCustomerByCode(customerCode);
+        if (customer.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("The customer with code %s was not found", customerCode));
+        }
+        return customerMapper.toDto(customer.get());
+    }
+}
