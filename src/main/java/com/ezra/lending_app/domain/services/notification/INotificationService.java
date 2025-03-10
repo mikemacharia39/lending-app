38e90e8b1ec0318dd3f4fee6179cd399d66ae5cc
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-10 10:21:07 +0300

    Implementation to send notifications

--- src/main/java/com/ezra/lending_app/domain/services/notification/INotificationService.java
+++ src/main/java/com/ezra/lending_app/domain/services/notification/INotificationService.java
@@ -1,8 +1,9 @@
-package com.ezra.lending_app.domain.services;
+package com.ezra.lending_app.domain.services.notification;
+
+import com.ezra.lending_app.api.dto.notification.NotificationDto;
 
 public interface INotificationService {
     // support sending notification via email, sms and push notification
     // define a method that will be overridden by the implementing class
-    void sendNotification(String message);
-
+    void sendNotification(NotificationDto notification);
 }
