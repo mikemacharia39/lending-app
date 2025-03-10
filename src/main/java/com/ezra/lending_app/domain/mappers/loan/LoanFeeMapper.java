f3f5a24f901867204b300c124e1def0f07b8a2f7
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-10 10:21:06 +0300

    Implementation to send notifications

--- src/main/java/com/ezra/lending_app/domain/mappers/loan/LoanFeeMapper.java
+++ src/main/java/com/ezra/lending_app/domain/mappers/loan/LoanFeeMapper.java
@@ -3,8 +3,13 @@ package com.ezra.lending_app.domain.mappers.loan;
 import com.ezra.lending_app.api.dto.loan.LoanFeeDto;
 import com.ezra.lending_app.domain.entities.LoanFee;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public abstract class LoanFeeMapper {
     public abstract LoanFeeDto dto(LoanFee loanFee);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "loan", ignore = true)
+    public abstract LoanFee entity(LoanFeeDto loanFeeDto);
 }
