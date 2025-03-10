c624bea3b82b3d8a74fa4f5b0b93f655a1d8e4df
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-10 14:04:16 +0300

    Define tables

--- src/main/java/com/ezra/lending_app/LendingApplication.java
+++ src/main/java/com/ezra/lending_app/LendingApplication.java
@@ -2,8 +2,10 @@ package com.ezra.lending_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LendingApplication {
 
 	public static void main(String[] args) {
