b956bc3cc5636caccdc3af1e8c2282b1b776f15b
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-10 16:47:09 +0300

    documentation of processes

--- src/main/java/com/ezra/lending_app/api/controllers/LoanController.java
+++ src/main/java/com/ezra/lending_app/api/controllers/LoanController.java
@@ -1,11 +1,43 @@
 package com.ezra.lending_app.api.controllers;
 
+import com.ezra.lending_app.api.dto.loan.LoanRequestDto;
+import com.ezra.lending_app.api.dto.loan.LoanResponseDto;
+import com.ezra.lending_app.domain.enums.LoanState;
+import com.ezra.lending_app.domain.services.LoanService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RequiredArgsConstructor
-@RequestMapping("loans")
+@RequestMapping("/loans")
 @RestController
 public class LoanController {
+
+    public final LoanService loanService;
+
+    @GetMapping("/products/{productCode}/customer/{customerCode}")
+    public LoanResponseDto checkLoanEligibility(@PathVariable String productCode, @PathVariable String customerCode) {
+        return loanService.checkLoanEligibility(productCode, customerCode);
+    }
+
+    @PostMapping("/products/{productCode}/customer/{customerCode}")
+    public LoanResponseDto applyForLoan(@PathVariable String productCode,
+                                        @PathVariable String customerCode,
+                                        @Valid @RequestBody LoanRequestDto loanRequestDto) {
+        return loanService.applyForLoan(productCode, customerCode, loanRequestDto);
+    }
+
+    @PutMapping("/{loanCode}/workflow")
+    public LoanResponseDto updateLoanStatus(@PathVariable String loanCode, @RequestParam LoanState status) {
+        return loanService.loanWorkflow(loanCode, status);
+    }
 }
