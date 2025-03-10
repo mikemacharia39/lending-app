0f33c87464a54028709c86a745f31eb9e9057218
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-10 10:21:09 +0300

    Implementation to send notifications

---
+++ src/main/java/com/ezra/lending_app/domain/services/notification/PushNotificationService.java
@@ -0,0 +1,15 @@
+package com.ezra.lending_app.domain.services.notification;
+
+import com.ezra.lending_app.api.dto.notification.NotificationDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PushNotificationService implements INotificationService {
+    @Override
+    public void sendNotification(NotificationDto notification) {
+        log.info("Sending push notification message: {} to recipient: {}",
+                notification.getMessage(), notification.getRecipient());
+    }
+}
