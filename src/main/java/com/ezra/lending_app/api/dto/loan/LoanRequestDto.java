f91144149c7473b62bb078b792f31f20e736721f
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-10 14:04:16 +0300

    Define tables

--- src/main/java/com/ezra/lending_app/api/dto/loan/LoanRequestDto.java
+++ src/main/java/com/ezra/lending_app/api/dto/loan/LoanRequestDto.java
@@ -1,10 +1,12 @@
 package com.ezra.lending_app.api.dto.loan;
 
+import com.ezra.lending_app.domain.enums.LoanTerm;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.math.BigDecimal;
+
 
 @Builder
 public record LoanRequestDto(
@@ -13,6 +15,8 @@ public record LoanRequestDto(
         @NotNull
         @Min(value = 1, message = "The loan amount must be greater than or equal to 1")
         BigDecimal requestedAmount,
+        @NotNull
+        LoanTerm loanTerm,
         @NotNull
         int loanPeriod
 ) {
