8af13482eace5974c1f4353139b9d9bce97c52fd
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-12 18:46:09 +0300

    Lets make this application work

--- src/main/java/com/ezra/lending_app/domain/entities/BaseEntity.java
+++ src/main/java/com/ezra/lending_app/domain/entities/BaseEntity.java
@@ -1,17 +1,26 @@
 package com.ezra.lending_app.domain.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
-
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@SuperBuilder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @MappedSuperclass
 public abstract class BaseEntity implements Serializable {
@@ -22,9 +31,20 @@ public abstract class BaseEntity implements Serializable {
 
     @CreatedDate
     @Column(name = "date_created", nullable = false, updatable = false, columnDefinition = "DATETIME")
-    private final Instant dateCreated = Instant.now();
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private Instant dateCreated = Instant.now();
 
     @LastModifiedDate
     @Column(name = "date_modified", columnDefinition = "DATETIME")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private Instant dateModified;
+
+    @PrePersist
+    @PreUpdate
+    void prePersist() {
+        if (this.dateCreated == null) {
+            this.dateCreated = Instant.now();
+        }
+        this.dateModified = Instant.now();
+    }
 }
