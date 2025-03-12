a0285407fa1699fdd6e27816a295f9ec09af50a2
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-12 18:46:08 +0300

    Lets make this application work

--- src/main/java/com/ezra/lending_app/api/dto/customer/CustomerRequestDto.java
+++ src/main/java/com/ezra/lending_app/api/dto/customer/CustomerRequestDto.java
@@ -3,7 +3,9 @@ package com.ezra.lending_app.api.dto.customer;
 import java.util.List;
 
 import com.ezra.lending_app.domain.enums.DeviceOSType;
+import com.ezra.lending_app.domain.enums.IdentificationType;
 import com.ezra.lending_app.domain.enums.NotificationChannel;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -20,6 +22,12 @@ public record CustomerRequestDto(
         @NotBlank
         @Size(min = 1, max = 50)
         String lastName,
+        @NotNull
+        IdentificationType identificationType,
+        @NotNull
+        @Size(min = 1, max = 30)
+        String identificationNumber,
+        @Email
         String email,
         @NotNull
         @NotBlank
@@ -30,6 +38,7 @@ public record CustomerRequestDto(
         List<CustomerAddressDto> addresses,
         String deviceId,
         DeviceOSType deviceType,
+        @NotNull
         NotificationChannel preferredNotificationChannel
 ) {
 }
