1a744444ba7d317e2ed58008cedf8ffb3f8ba671
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-12 18:46:12 +0300

    Lets make this application work

--- src/main/java/com/ezra/lending_app/domain/entities/ProductFee.java
+++ src/main/java/com/ezra/lending_app/domain/entities/ProductFee.java
@@ -10,16 +10,18 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Builder
+@Setter
+@SuperBuilder(toBuilder = true)
 @Entity
 @Table(name = "fee")
 public class ProductFee extends BaseEntity {
