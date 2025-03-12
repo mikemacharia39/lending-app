be7b9c46067f888a309600f6ef7f1354d0d07eba
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-12 18:46:10 +0300

    Lets make this application work

--- src/main/java/com/ezra/lending_app/domain/entities/CustomerAddress.java
+++ src/main/java/com/ezra/lending_app/domain/entities/CustomerAddress.java
@@ -6,12 +6,16 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@Setter
+@Getter
+@SuperBuilder(toBuilder = true)
 @Entity
 @Table(name = "customer_address")
 public class CustomerAddress extends BaseEntity {
