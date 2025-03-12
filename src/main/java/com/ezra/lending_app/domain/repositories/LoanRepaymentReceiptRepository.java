58d74000d0dd155ad63e53188f2478de3eeb2ed6
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-12 18:46:14 +0300

    Lets make this application work

--- src/main/java/com/ezra/lending_app/domain/repositories/LoanRepaymentReceiptRepository.java
+++ src/main/java/com/ezra/lending_app/domain/repositories/LoanRepaymentReceiptRepository.java
@@ -2,12 +2,14 @@ package com.ezra.lending_app.domain.repositories;
 
 import com.ezra.lending_app.domain.entities.LoanRepaymentReceipt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface LoanRepaymentReceiptRepository extends JpaRepository<LoanRepaymentReceipt, Long> {
-    // find all loan repayment by loan id
+
+    @Query("SELECT lrr FROM LoanRepaymentReceipt lrr WHERE lrr.loan.id = :loanId")
     List<LoanRepaymentReceipt> findAllByLoanId(Long loanId);
 }
