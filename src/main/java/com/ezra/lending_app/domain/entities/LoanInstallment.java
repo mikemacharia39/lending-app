21b13e3be1f1678302da8457c2932379190fe85b
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-12 18:46:11 +0300

    Lets make this application work

--- src/main/java/com/ezra/lending_app/domain/entities/LoanInstallment.java
+++ src/main/java/com/ezra/lending_app/domain/entities/LoanInstallment.java
@@ -1,21 +1,21 @@
 package com.ezra.lending_app.domain.entities;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
-import java.time.Instant;
+import lombok.experimental.SuperBuilder;
 
 import static com.ezra.lending_app.domain.util.RandomReferenceGenerator.generateReference;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@SuperBuilder(toBuilder = true)
 @Entity
 @Table(name = "loan_installment")
 public class LoanInstallment extends BaseEntity {
