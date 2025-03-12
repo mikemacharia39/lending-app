c0f0816ccdeda9306a2bf8f2f5b9837414b5fdf7
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-12 18:46:12 +0300

    Lets make this application work

--- src/main/java/com/ezra/lending_app/domain/mappers/customer/CustomerAddressMapper.java
+++ src/main/java/com/ezra/lending_app/domain/mappers/customer/CustomerAddressMapper.java
@@ -2,8 +2,15 @@ package com.ezra.lending_app.domain.mappers.customer;
 
 import com.ezra.lending_app.api.dto.customer.CustomerAddressDto;
 import com.ezra.lending_app.domain.entities.CustomerAddress;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(componentModel = "spring")
 public abstract class CustomerAddressMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "dateCreated", ignore = true)
+    @Mapping(target = "dateModified", ignore = true)
     abstract CustomerAddress toEntity(CustomerAddressDto customerAddressDto);
+
     abstract CustomerAddressDto toDto(CustomerAddress customerAddress);
 }
