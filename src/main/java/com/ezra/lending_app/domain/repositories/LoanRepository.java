a42a624071c03984ebe1452821ac814cbcffeeb3
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-12 18:46:14 +0300

    Lets make this application work

--- src/main/java/com/ezra/lending_app/domain/repositories/LoanRepository.java
+++ src/main/java/com/ezra/lending_app/domain/repositories/LoanRepository.java
@@ -3,6 +3,7 @@ package com.ezra.lending_app.domain.repositories;
 import com.ezra.lending_app.domain.entities.Loan;
 import com.ezra.lending_app.domain.enums.LoanState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,9 +11,10 @@ import java.util.Optional;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
-    Optional<Loan> findByLoanCode(String loanCode);
+    Optional<Loan> findByCode(String loanCode);
 
-    List<Loan> findAllByCustomerCode(String customerCode);
+    @Query("SELECT l FROM Loan l WHERE l.customer.code = :customerCode")
+    List<Loan> findAllLoansByCustomerCode(String customerCode);
 
     List<Loan> findAllByStateIn(List<LoanState> states);
 }
