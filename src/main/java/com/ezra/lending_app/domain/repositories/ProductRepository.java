1afd9b665e11fa636d5699f3ca0539af70003f30
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-12 18:46:14 +0300

    Lets make this application work

--- src/main/java/com/ezra/lending_app/domain/repositories/ProductRepository.java
+++ src/main/java/com/ezra/lending_app/domain/repositories/ProductRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Optional<Product> findByProductCode(String productCode);
+    Optional<Product> findByCode(String productCode);
 }
