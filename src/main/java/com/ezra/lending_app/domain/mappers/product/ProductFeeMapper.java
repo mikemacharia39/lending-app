b5460ee96a773ea3f92765ba4e77a10664df63ea
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-12 18:46:13 +0300

    Lets make this application work

--- src/main/java/com/ezra/lending_app/domain/mappers/product/ProductFeeMapper.java
+++ src/main/java/com/ezra/lending_app/domain/mappers/product/ProductFeeMapper.java
@@ -1,15 +1,17 @@
 package com.ezra.lending_app.domain.mappers.product;
 
-import com.ezra.lending_app.api.dto.product;
+import com.ezra.lending_app.api.dto.product.ProductFeeDto;
 import com.ezra.lending_app.domain.entities.ProductFee;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public abstract class ProductFeeMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "dateCreated", ignore = true)
     @Mapping(target = "dateModified", ignore = true)
-    abstract ProductFee toEntity(product.ProductFeeDto productFeeDto);
-    abstract product.ProductFeeDto toDto(ProductFee productFee);
+    @Mapping(target = "product", ignore = true)
+    abstract ProductFee toEntity(ProductFeeDto productFeeDto);
+
+    abstract ProductFeeDto toDto(ProductFee productFee);
 }
