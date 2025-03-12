aa5b83718da86e5d791b1551cd8b536315f7a32e
mikemacharia39 <mikemacharia39@gmail.com>
2025-03-12 22:05:26 +0300

    Make the loan application work

--- src/main/java/com/ezra/lending_app/domain/entities/LoanFee.java
+++ src/main/java/com/ezra/lending_app/domain/entities/LoanFee.java
@@ -12,11 +12,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 @SuperBuilder(toBuilder = true)
 @Entity
 @Table(name = "loan_fee")
